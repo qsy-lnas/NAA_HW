8750bf66a264d6a09e8cae1acdea1f528a52b173
qsy-lnas <lnasqsy@qq.com>
2021-10-23 00:25:30 +0800

    PJ1_1_coderun

--- 大作业1/problem_1.py
+++ 大作业1/problem_1.py
@@ -3,34 +3,111 @@ import matplotlib
 from PIL import Image
 import math 
 
-sourceim_path = "qrcode_polar.png"
-sourceim_type = "png"
-targetim_path = "qrcode_carts.png"
-targetim_type = "png"
-
-
-im = Image.open(sourceim_path)
-source_image = np.array(im)
-image_shape = source_image.shape
-target_image = np.zeros(image_shape)
-for i in range(np.size(target_image)):
-    pass
 
 def rtheta2xy(r, theta):
     x = 2 * math.pi - r * math.sin(theta)
     y = 2 * math.pi + r * math.cos(theta)
-    return (x, y)
+    if x == 4 * math.pi:
+        x -= 0.0001
+    elif y == 4 * math.pi:
+        y -= 0.0001
+    return x, y
 
-#x, y \in [0, 4pi] to pixel \in 474, 474(double)
-#r != 2pi
-def source_xy2pix(x, y):
-    x = x / (4 * math.pi) * image_shape[0]
-    y = y / (4 * math.pi) * image_shape[1]
-    return (x, y)
+def source_xy2pix(x, y, shape):
+    '''
+    x, y \in [0, 4pi](double) to pixel \in [474, 474](double)
+    
+    r != 2pi
+    --------
+    '''
+    
+    x = x / (4 * math.pi) * shape[0]
+    y = y / (4 * math.pi) * shape[1]
+    return x, y
 
-def target_pix2xy(x, y):
-    x = x / image_shape[0] * 2 * math.pi
-    y = y / image_shape[1] * 2 * math.pi
+def target_pix2xy(x, y, shape):
+    '''
+    pixel \in [474, 474](int) to x, y \in [0, 2\pi](double)
+    
+    '''
+    x = x / shape[0] * 2 * math.pi
+    y = y / shape[1] * 2 * math.pi
+    return x, y
 
+sourceim_path = "qr_polar.jpg"
+sourceim_type = "jpg"
+targetim_path = "qr_carts.jpg"
+targetim_type = "jpg"
 
-print(target_image.shape)
+im = Image.open(sourceim_path)
+source_image = np.array(im)
+image_shape = source_image.shape
+#print(image_shape, np.size(source_image))
+target_image = np.zeros(image_shape)
+for i in range(image_shape[0] * image_shape[1]):
+    '''get pix_x, pix_y of this pixel in target figure'''
+    pix_x = i // image_shape[1]
+    pix_y = i % image_shape[1]
+    '''change the coordinate to length of x, y(double)
+    r, theta = x, y'''
+    x, y = target_pix2xy(pix_x, pix_y, image_shape)
+    '''change the r, theta to coordinate of x, y'''
+    x, y = rtheta2xy(x, y)
+    '''change the x, y to pix_x, pix_y in source_image'''
+    x, y = source_xy2pix(x, y, image_shape)
+    #if x >= 474 or y >= 474:
+    #    print("pix_x = %d, pix_y = %d"%(pix_x, pix_y))
+    #    print("x = %f, y = %f"%(x, y))
+    if x % 1 == 0 and y % 1 == 0:
+        '''pixel locate on pixel'''
+        target_image[int(pix_x), int(pix_y)] = \
+            source_image[int(math.floor(x)), \
+                math.floor(int(y))]
+    else:
+        x0 = int(math.floor(x))
+        x1 = int(math.ceil(x))
+        y0 = int(math.floor(y))
+        y1 = int(math.ceil(y))
+        if x1 == 474: x1 = 473
+        if y1 == 474: y1 = 473
+        '''for debugging of 474 outof range'''
+        #if x0 >= 474 or x1 >= 474 or y0 >= 474 or y1 == 474:
+        #    print("x0 = %d, x1 = %d, y0 = %d, y1 = %d"\
+        #        %(x0, x1, y0, y1))
+        #    print("pix_x = %d, pix_y = %d, x = %f, y = %f"%\
+        #        (pix_x, pix_y, x, y))
+        if x % 1 == 0:
+            '''single linear interpolation in y'''
+            p = np.dot((y - y0), source_image[x0, y1]) \
+                + np.dot((y1 - y),  source_image[x0, y0])
+        elif y % 1 == 0:
+            '''single linear interpolation in x'''
+            p = np.dot((x - x0), source_image[x1, y0]) \
+                + np.dot((x1 - x), source_image[x0, y0])
+        else:
+            '''interpolation in x'''
+            p0 = np.dot((y - y0), source_image[x0, y1])\
+                + np.dot((y1 - y), source_image[x0, y0])
+            p1 = np.dot((y - y0), source_image[x1, y1])\
+                + np.dot((y1 - y), source_image[x1, y0])
+            '''interpolation in y'''
+            p = np.dot(p1, (x - x0)) + np.dot(p0, (x1 - x))
+        #if pix_x >= 474 or pix_y >= 474:
+        #    print("pix_x = %d, pix_y = %d"%(pix_x, pix_y))
+        target_image[int(pix_x), int(pix_y)] = p
+#print(target_image)
+#print(type(target_image))
+'''map t_i to 0-255(int)'''
+result_image = np.zeros(image_shape)
+for i in range(image_shape[0] * image_shape[1]):
+    x = i // image_shape[1]
+    y = i % image_shape[1]
+    if sum(target_image[x][y]) > 1.5 * 255:
+        result_image[x][y] = 255
+    else:
+        result_image[x][y] = 0
+im = Image.fromarray(target_image[:, :, 0], 'RGB')
+im.save(targetim_path)
+print("Convert successfully")
+            
+
---
+++ 大作业1/test.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import numpy as np
+import math
+
+x = 473
+x = x / 474 * 2 * math.pi
+print(x)
